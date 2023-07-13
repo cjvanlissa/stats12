568041c6cf1803627fb4f657eaa5aa9146d0394a
cjvanlissa <c.j.vanlissa@tilburguniversity.edu>
2023-07-13 08:33:23 +0200

    update 2023-07-13 08:33:22.835784

--- R/my_setup.R
+++ R/my_setup.R
@@ -1,4 +1,30 @@
 # book-specific code to include on every page
+add_mcs <- function(filename){
+  qs <- read.csv(filename, stringsAsFactors = FALSE, header = FALSE)
+  cat('::: {.webex-check .webex-box}\n\n')
+  for(i in 1:nrow(qs)){
+    cat(paste0("**Question ", i, "**\n\n"))
+    Args <- as.vector(unname(qs[i,]))
+    Args <- Args[!(is.na(Args) | Args == "")]
+    Args <- trimws(Args[-length(Args)])
+    Args <- as.list(Args)
+    do.call(mc, args = Args)
+    cat("\n\n")
+  }
+  cat(":::\n\n")
+  cat(hide("Show explanations"))
+  for(i in 1:nrow(qs)){
+    cat(paste0("**Question ", i, "**\n\n"))
+    Args <- qs[i,]
+    Args <- Args[!(is.na(Args) | Args == "")]
+    Args <- as.character(Args)
+    cat(tail(Args, 1))
+    cat("\n\n")
+  }
+  cat(unhide())
+  cat('\n\n')
+}
+
 mc <- function(question,
                answer,
                ...){
